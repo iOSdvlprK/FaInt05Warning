17513864e4536d2cb72e28017798837f3344765a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-04-12 22:48:07 +0900

    Initial Commit

---
+++ FaInt05Warning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaInt05Warning
+//
+//  Created by joe on 4/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
